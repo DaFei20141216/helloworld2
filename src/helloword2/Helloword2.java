041e7c342aca8a6702eb2ab469e7072d510fdb6f
DaFei20141216 <1586893470@qq.com>
2021-01-08 09:10:23 +0800

    test2

--- src/helloword2/Helloword2.java
+++ src/helloword2/Helloword2.java
@@ -3,7 +3,7 @@ package helloword2;
 public class Helloword2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World,Ëæ±ã¸ÄÒ»¸Ä");
 
 	}
 
